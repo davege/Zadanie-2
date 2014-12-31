d0b0a0eeb72e438242b853b442f96ae1471ac911
dawidglosny <dawidglosny@gmail.com>
2014-12-31 15:55:57 +0100

    Zadanie 2 i dodatkowe

---
+++ src/app_one.js
@@ -0,0 +1,35 @@
+AppOne = angular.module('AppOne', ['ui.router']); 
+AppOne.controller('NameController', ['$scope', function($scope){
+$scope.name = 'Dawid'; 
+}]);
+
+//zadanie2
+AppOne.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/name");
+
+  $stateProvider
+    .state('name', {
+      url: "/name",
+      templateUrl: "src/ptrng.tmplt.html",
+      controller: function($scope) {
+        $scope.name = 'Dawid';
+      }
+    })
+    //dodatkowe
+	.state('Cat', {
+      url: "/Cat",
+      templateUrl: "src/Cat.html"
+	  })
+	
+	
+	
+});
+
+AppOne.directive('ptrngName',function()
+{
+return{
+restrict: 'E',
+template: 'Your name is {{name}}'
+};
+});
